c930c967bf4d06d298fe4d27d6600437e7463322
Kathy Murdoch <kathy@nivan.net>
2024-07-02 14:57:19 +0100

    initial fiddling around

---
+++ stcr/__init__.py
@@ -0,0 +1,64 @@
+import os
+
+from flask import Flask, redirect, url_for
+from flask import render_template
+from flask_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
+
+app = Flask(__name__)
+
+exec(open('secrets.py').read())  #provides `config`
+
+app.secret_key = config['flask_secret_key']
+# OAuth2 must make use of HTTPS in production environment.
+#os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"      # !! Only in development environment.
+
+app.config["DISCORD_CLIENT_ID"] = config['client_id']    # Discord client ID.
+app.config["DISCORD_CLIENT_SECRET"] = config['client_secret']                # Discord client secret.
+app.config["DISCORD_REDIRECT_URI"] = "https://stcr.nevira.net/auth/discord-redirect"                 # URL to your callback endpoint.
+app.config["DISCORD_BOT_TOKEN"] = ""                    # Required to access BOT resources.
+
+
+discord = DiscordOAuth2Session(app)
+
+def welcome_user(user):
+    dm_channel = discord.bot_request("/users/@me/channels", "POST", json={"recipient_id": user.id})
+    return discord.bot_request(
+        f"/channels/{dm_channel['id']}/messages", "POST", json={"content": "Thanks for authorizing the app!"}
+    )
+
+@app.route("/auth/discord-login/")
+def login():
+    return discord.create_session(scope=['identify', 'email'])
+
+
+@app.route("/auth/discord-redirect")
+def callback():
+    discord.callback()
+    user = discord.fetch_user()
+    welcome_user(user)
+    return redirect(url_for(".me"))
+
+
+@app.errorhandler(Unauthorized)
+def redirect_unauthorized(e):
+    return redirect(url_for("login"))
+
+
+@app.route("/me/")
+@requires_authorization
+def me():
+    user = discord.fetch_user()
+    return f"""
+    <html>
+        <head>
+            <title>{user.name}</title>
+        </head>
+        <body>
+            <img src='{user.avatar_url}' />
+            <pre>{user}</pre>
+        </body>
+    </html>"""
+
+
+if __name__ == "__main__":
+    app.run()
